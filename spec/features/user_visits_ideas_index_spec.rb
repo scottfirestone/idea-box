f24f78e9e5368f8090385f9e301f0b2d15efccdd
Scott Firestone <scott.firestone.dev@gmail.com>
2016-05-09 14:55:41 -0600

    Create form for creating new idea
    Tested in feature test. Currently uses rails form helper

--- spec/features/user_visits_ideas_index_spec.rb
+++ spec/features/user_visits_ideas_index_spec.rb
@@ -5,12 +5,13 @@ RSpec.feature "User visits ideas index" do
     visit "/ideas"
 
     expect(current_path).to eq("/ideas")
-    expect(page).to have_content("Ideas")
-    within("#new-idea") do
-      expect(page).to have_content("Add a new idea")
+    within(".ideas") do
+      expect(page).to have_content("Ideas")
+    end
+    within(".new-idea") do
       expect(page).to have_content("Title")
       expect(page).to have_content("Body")
-      expect(page).to have_submit_button("Save")
+      expect(page).to have_button("Save")
     end
   end
 end
