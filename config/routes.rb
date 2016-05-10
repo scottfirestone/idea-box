e0223b00239f854d9622923f39bfab6d28eef219
Scott Firestone <scott.firestone.dev@gmail.com>
2016-05-10 15:15:05 -0600

    Create namespaced routes for api

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   root          to: "ideas#index"
   get "/ideas", to: "ideas#index"
-  resources :ideas, except: :index, defaults: {format: :json}
+
+  namespace :api do
+    namespace :v1 do
+      resources :ideas, only: [:index, :create]
+    end
+  end
 end
