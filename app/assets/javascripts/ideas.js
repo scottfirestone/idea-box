425515ac9707f6f45399b89485be39b65a4ca625
Scott Firestone <scott.firestone.dev@gmail.com>
2016-05-09 23:58:13 -0600

    Refactor new idea inputs and write ajax call to create idea

--- app/assets/javascripts/ideas.js
+++ app/assets/javascripts/ideas.js
@@ -1,7 +1,30 @@
 'use strict';
 
 $('document').ready(function(){
-  $('#add-idea').on("click", function() {
-    $('#new-idea').slideDown(350);
+  $('#add-idea').on("click", createIdea);
+});
+
+function createIdea(){
+  var ideaParams = {idea: {title: $('#idea-title').val(),
+                           body: $('#idea-body').val()}}
+  $.ajax({
+    url: "/ideas",
+    method: "POST",
+    dataType: "json",
+    data: ideaParams,
+    success: function(post){
+      $("#ideas").renderIdea(idea);
+    }
   });
-})
+}
+
+function renderIdea(idea){
+  $("#ideas")
+    .append("<div class=idea data-idea-id="
+    + idea.id
+    + "><p>"
+    + idea.title
+    + "</p><p>"
+    + idea.title
+    + "</p></div>");
+}
--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,11 +1,18 @@
 class IdeasController < ApplicationController
-
+  respond_to :js
 
   def index
     @idea = Idea.new
   end
 
   def create
-
+    @idea = Idea.create(idea_params)
+    respond_with @idea
   end
+
+  private
+
+    def idea_params
+      params.require(:idea).permit(:title, :body)
+    end
 end
