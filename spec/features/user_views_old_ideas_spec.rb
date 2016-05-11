8ff3d5c9d8c3273ab6b6dc6cc30bb8214dd61981
Scott Firestone <scott.firestone.dev@gmail.com>
2016-05-10 18:18:55 -0600

    Test broken...
    Test isn't passing but works in development.
    Finishing commit to merge branch and continue project progress

--- spec/features/user_views_old_ideas_spec.rb
+++ spec/features/user_views_old_ideas_spec.rb
@@ -1,11 +1,11 @@
 require "rails_helper"
 
-RSpec.feature "User views old ideas" do
+RSpec.feature "User views old ideas", js: true do
   scenario "they see the previously created ideas" do
     idea = create(:idea)
-binding.pry
+
     visit root_path
-save_and_open_page
+
     within("#ideas") do
       expect(page).to have_content(idea.title)
       expect(page).to have_content(idea.body)
