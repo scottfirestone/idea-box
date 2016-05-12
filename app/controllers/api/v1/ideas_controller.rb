4a5764ca4745da4ce36c973567acef9a5725fbc3
Scott Firestone <scott.firestone.dev@gmail.com>
2016-05-11 22:05:35 -0600

    Add ordering in api index action
    Ideas ordered by id desc, most recent listed first

--- app/controllers/api/v1/ideas_controller.rb
+++ app/controllers/api/v1/ideas_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::IdeasController < ApplicationController
   respond_to :json
 
   def index
-    respond_with Idea.all, location: nil
+    respond_with Idea.all.order('id desc'), location: nil
   end
 
   def create
