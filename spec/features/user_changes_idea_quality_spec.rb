ef60625707ff34b87cfe975d0f8010278b3173e6
Scott Firestone <scott.firestone.dev@gmail.com>
2016-05-12 10:03:50 -0600

    Pass test for downvote idea feature spec

--- spec/features/user_changes_idea_quality_spec.rb
+++ spec/features/user_changes_idea_quality_spec.rb
@@ -19,4 +19,23 @@ RSpec.feature "User changes idea quality", js: true do
     expect(page).to_not have_content("Swill")
     expect(page).to_not have_content("Plausible")
   end
+
+  scenario "the quality is downvoted" do
+    idea = create(:idea)
+
+    idea.update(quality: "Genius")
+
+    visit root_path
+    find('span.downvote').click
+
+    expect(page).to have_content("Plausible")
+    expect(page).to_not have_content("Swill")
+    expect(page).to_not have_content("Genius")
+
+    find('span.downvote').click
+
+    expect(page).to have_content("Swill")
+    expect(page).to_not have_content("Genius")
+    expect(page).to_not have_content("Plausible")
+  end
 end
